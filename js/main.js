b534a8f8ec7c7ab43ddebefe24820ffcc7905959
needlzzz <marc@marcdk.ch>
2021-02-09 21:55:25 +0100

    added sw worker (final)

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("../sw_cached_pages.js")
+      .register("../sw_cached_wholeSite.js")
       .then((reg) => console.log("Service Worker: Registered"))
       .catch((err) => console.log(`Service Worker: Error: ${err}`));
   });
--- sw_cached_wholeSite.js
+++ sw_cached_wholeSite.js
@@ -1,18 +1,10 @@
 const cacheName = "v2";
 
-
-
 //installation of the sw
 //call Install event
 
 self.addEventListener("install", (e) => {
   console.log("Service Worker: Installed");
-
-
-      .then(() => {
-        self.skipWaiting();
-      })
-  );
 });
 
 //activation of the sw
@@ -38,5 +30,19 @@ self.addEventListener("activate", (e) => {
 //call Fetch Event
 self.addEventListener("fetch", (e) => {
   console.log("Service Worker: Fetching");
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.requestß)));
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        // Make copy/clone of response
+        const resClone = res.clone();
+        // Open Cache
+        caches.open(cacheName).then((cache) => {
+          //Add response to cache
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch((err) => caches.match(e.request))
+      .then((res) => res)
+  );
 });
