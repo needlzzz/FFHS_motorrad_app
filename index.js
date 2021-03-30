4f400df2319f05fd961aa0d950e05655ba75bc94
Roger <roger.abegg@students.ffhs.ch>
2021-03-30 17:40:11 +0200

    include new route for prot. dashboard & verify jwt

--- index.js
+++ index.js
@@ -7,21 +7,24 @@ dotenv.config();
 
 // connect to db
 mongoose.connect(
-    process.env.DB_CONNECT,
-    {
+  process.env.DB_CONNECT,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    },
-    () => console.log("connected to db")
+  },
+  () => console.log("connected to db")
 );
 
 // import routes
 const authRoutes = require("./routes/auth");
+const dashboardRoutes = require("./routes/dashboard");
+const verifyToken = require("./routes/validate-token");
 
 // middlewares
 app.use(express.json()); // for body parser
 
 // route middlewares
 app.use("/api/user", authRoutes);
+app.use("/api/dashboard", verifyToken, dashboardRoutes);
 
-app.listen(3000, () => console.log("server is running"));
+app.listen(3000, () => console.log("server is running..."));
