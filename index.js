a7e005e1729b23763d342dd186e9dfed77d0de78
Roger <roger.abegg@students.ffhs.ch>
2021-03-30 10:51:47 +0200

    add express route, include dotenv, connect via mon

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+dotenv.config();
+// connect to db
+mongoose.connect(
+process.env.DB_CONNECT,
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+},
+() => console.log("connected to db")
+);
+
+// import routes
+const authRoutes = require("./routes/auth");
+// middlewares
+app.use(express.json()); // for body parser
+// route middlewares
+app.use("/api/user", authRoutes);
+app.listen(3000, () => console.log("server is running..."));
