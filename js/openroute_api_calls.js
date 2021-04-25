802aa73663a1533f34de60eb2788abd9413d2d03
needlzzz <marc@marcdk.ch>
2021-04-25 23:00:58 +0200

    added mapbox map for testing purposes

--- js/openroute_api_calls.js
+++ js/openroute_api_calls.js
@@ -41,14 +41,12 @@ console.log(route1Zurich);
     let url = "";
     let route1 =
       "https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf6248b53c2e2b177c42258f8caf73f94745ae&start=8.681495,49.41461&end=8.687872,49.420318";
-
     openrouteXHR.open("GET", route1);
     openrouteXHR.setRequestHeader(
       "Accept",
       "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8"
     );
     openrouteXHR.send();
-
     openrouteXHR.onreadystatechange = () => {
       if (openrouteXHR.readyState === 4 && openrouteXHR.status === 200) {
         resolve(openrouteXHR.responseText);
