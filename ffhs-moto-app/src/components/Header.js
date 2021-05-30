765a7c3a7904208e79c8e6814a9a78b4ed184871
needlzzz <marc@marcdk.ch>
2021-05-30 17:15:34 +0200

    corrected type in Header.js for profile.png

--- ffhs-moto-app/src/components/Header.js
+++ ffhs-moto-app/src/components/Header.js
@@ -1,9 +1,9 @@
-import React, {useContext, useState} from 'react'; // import hooks from React
+import React, { useContext, useState } from 'react'; // import hooks from React
 import '../css/style.css'; // import CSS
 import { Link } from 'react-router-dom'; //import Link
-import { AppContext } from "./Context"; // import Context component
+import { AppContext } from './Context'; // import Context component
 import motorbike2 from '../img/motorbike2.png'; // import images
-import profileimg from '../img/profile.png'; // import images
+import profileimg from '../img/Profile.png'; // import images
 import burgerimg from '../img/burgermenu.png'; // import images
 
 // create Header component
@@ -11,69 +11,79 @@ const Header = () => {
   // access "global" state object by useContext
   const myContext = useContext(AppContext);
   // define burger state
-  const [mobile, setMobile] = useState("");
-  
+  const [mobile, setMobile] = useState('');
+
   // burger-menu function for responsiveness
   const Burger = () => {
-    if(mobile==="") {setMobile('mobile')}
-    else {
-      setMobile('')
+    if (mobile === '') {
+      setMobile('mobile');
+    } else {
+      setMobile('');
     }
-
-  }
-  
+  };
 
   // create Login request, setStates with received data
   const Logout = () => {
     fetch('http://localhost:3000/api/user/logout', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        credentials: 'include', // send a request with credentials included on same-origin and cross-origin calls
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include', // send a request with credentials included on same-origin and cross-origin calls
     })
-    .then(response => response.json())
-    .then(json => {
-      if(json.error===null) {
-        console.log(json);
-        myContext.setLoggedin(false);
-        myContext.setRegistered(null)
-      }   
-    })
-    .catch(err => console.log(err));
-  }
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.error === null) {
+          console.log(json);
+          myContext.setLoggedin(false);
+          myContext.setRegistered(null);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <React.Fragment>
       <div id='menu_top'>
-        <Link id="logolink" to='/home'>
+        <Link id='logolink' to='/home'>
           <img id='logo' img src={motorbike2} alt='Homepage logo'></img>
-        </Link>   
-        {(myContext.loggedin !== false) ? (
+        </Link>
+        {myContext.loggedin !== false ? (
           <>
             <Link to='/login'>
-              <img id="profileimg" src={profileimg} alt="Profile" />
+              <img id='profileimg' src={profileimg} alt='Profile' />
             </Link>
-            
+
             <Link to='/login'>
-              <button onClick={Logout}  className="login-btn" type='button'>
+              <button onClick={Logout} className='login-btn' type='button'>
                 Logout
               </button>
             </Link>
           </>
         ) : (
           <Link to='/login'>
-              <button className="login-btn" type='button'>
-                Login
-              </button>
-          </Link>        
+            <button className='login-btn' type='button'>
+              Login
+            </button>
+          </Link>
         )}
       </div>
       <nav className='menu' id='myMenu'>
-        <Link className={`menulink ${mobile}`} to='/home'>Home</Link>
-        <Link className={`menulink ${mobile}`} to='/routes'>Route search</Link>
-        <Link className={`menulink ${mobile}`} to='/contact'>Contact form</Link>
-        <img id="burger" onClick={Burger} src={burgerimg} alt='Burgermenu'></img>
+        <Link className={`menulink ${mobile}`} to='/home'>
+          Home
+        </Link>
+        <Link className={`menulink ${mobile}`} to='/routes'>
+          Route search
+        </Link>
+        <Link className={`menulink ${mobile}`} to='/contact'>
+          Contact form
+        </Link>
+        <img
+          id='burger'
+          onClick={Burger}
+          src={burgerimg}
+          alt='Burgermenu'
+        ></img>
       </nav>
     </React.Fragment>
   );
--- ffhs-moto-app/src/components/Home.js
+++ ffhs-moto-app/src/components/Home.js
@@ -4,11 +4,6 @@ import { AppContext } from './Context'; // import Context component
 import LoginForm from './LoginForm'; // import component
 import Profile from './Profile'; // import component
 import { Link } from 'react-router-dom'; //import Link
-import {
-  asyncAPIandBackendCall,
-  APIresponse,
-  fetchDataFromAPI,
-} from '../js/openroute_api_calls';
 
 // create hook
 const LoginBody = () => {
@@ -18,15 +13,10 @@ const LoginBody = () => {
   // local state
   const [error, setError] = useState('');
 
-  import React from 'react'; // import hooks from React
-  import '../css/style.css'; // import CSS
-
   // if user.email state is not empty show text and logout, else show login form
   return (
     <React.Fragment>
-      <div className='App'>
-        <button onClick={asyncAPIandBackendCall}>Click here</button>
-      </div>
+      <div className='App'></div>
     </React.Fragment>
   );
 };
